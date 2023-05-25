8be37ae75d39a5340ee280263f92826a92b7d0b8
Tazim381 <jucse28.381@gmail.com>
2023-05-25 20:26:04 +0600

    image vault added

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {},
+    colors: {
+      "top-nav": "#232228",
+      "side-nav": "#35353d",
+      "white": "#FFFF",
+      "body": "#3c3c44",
+      "black":"#000000",
+      "search":"#25252A"
+    },
+  },
+  plugins: [],
+};
